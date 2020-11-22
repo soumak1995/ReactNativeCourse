c19175d2346a4c732ea903373dc504880b88da29
unknown <Soumak Maji>
2020-11-22 22:33:12 +0530

    Redux Adding Favorites

--- components/DIshdetailComponent.js
+++ components/DIshdetailComponent.js
@@ -5,12 +5,17 @@ import { DISHES } from '../shared/dishes';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import { Icon } from 'react-native-elements';
+import { postFavorite } from '../redux/ActionCreators';
 const mapStateToProps = state => {
     return {
       dishes: state.dishes,
-      comments: state.comments
+      comments: state.comments,
+      favorites: state.favorites
     }
   }
+  const mapDispatchToProps = dispatch => ({
+    postFavorite: (dishId) => dispatch(postFavorite(dishId))
+})
 function RenderComments(props) {
 
     const comments = props.comments;
@@ -73,14 +78,14 @@ class Dishdetail extends Component {
         title: 'Dish Details'
     };
     markFavorite(dishId) {
-        this.setState({favorites: this.state.favorites.concat(dishId)});
+        this.props.postFavorite(dishId);
     }
     render() {
         const dishId = this.props.navigation.getParam('dishId','');
         return(
             <ScrollView>
             <RenderDish dish={this.props.dishes.dishes[+dishId]}
-                favorite={this.state.favorites.some(el => el === dishId)}
+               favorite={this.props.favorites.some(el => el === dishId)}
                 onPress={() => this.markFavorite(dishId)} 
                 />
             <RenderComments comments={this.props.comments.comments.filter((comment) => comment.dishId === dishId)} />
@@ -89,4 +94,4 @@ class Dishdetail extends Component {
     }
 }
 
-export default connect(mapStateToProps)(Dishdetail);
+export default connect(mapStateToProps,mapDispatchToProps)(Dishdetail);
--- redux/configureStore.js
+++ redux/configureStore.js
@@ -5,7 +5,7 @@ import { dishes } from './dishes';
 import { comments } from './comments';
 import { promotions } from './promotions';
 import { leaders } from './leaders';
-
+import { favorites } from './favorites';
 export const ConfigureStore = () => {
     const store = createStore(
         combineReducers({
